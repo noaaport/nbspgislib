494d84c5d978c3c51365ae2833eadc2798def827
Jose F Nieves <nieves@volkron.net>
2015-01-02 19:28:10 -0400

    Corrected ifdef statements (caught by freebsd compiler)

--- src/common/unz.h
+++ src/common/unz.h
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2005 Jose F. Nieves <nieves@ltp.upr.clu.edu>
+ * Copyright (c) 2005 Jose F. Nieves <nieves@ltp.uprrp..edu>
  *
  * See LICENSE
  *
  * $Id$
  */
 #ifndef UNZ_H
-#define UNZ_h
+#define UNZ_H
 
 int unz(char *out, int *outlen, char *in, int inlen);
 int zip(char **out, int *outlen, char *in, int inlen, int level);
